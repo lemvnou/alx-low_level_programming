622eba49170833f1499a6b3339d7ad42ffbe99ba
lemvnou <ledeveloppeurlobi@gmail.com>
2022-09-15 18:31:46 -0700

    New Commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_isupper - check main
+ *@c: an integer input n
+ *Description: function that checks for uppercase character
+ *Return: 1 or 0.
+ */
+int _isupper(int c)
+
+{
+	char a;
+	int j = 0;
+
+	for (a = 'A'; a <= 'Z'; a++)
+		if (a == c)
+		{
+			j = 0;
+			break;
+		}
+	}
+	return (n);
+}
